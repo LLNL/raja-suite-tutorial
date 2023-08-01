b79fe37d344562b63797af0ca20981aa0bce699f
Arturo Vargas <artvar3@yahoo.com>
2023-08-01 16:26:26 -0700

    Update Intermediate_Tutorial/01-LOOP/fractal-ex1-RAJA-loop.cpp

--- Intermediate_Tutorial/01-LOOP/fractal-ex1-RAJA-loop.cpp
+++ Intermediate_Tutorial/01-LOOP/fractal-ex1-RAJA-loop.cpp
@@ -30,7 +30,7 @@ int main(int argc, char *argv[])
   printf("computing %d by %d fractal with a maximum depth of %d\n", width, width, maxdepth);
 
   //TODO: Create an Umpire QuickPool allocator with Unified Memory that will hold the
-  // pixels of the fractal image.
+  //pixels of the fractal image.
   auto& rm = umpire::ResourceManager::getInstance();
   unsigned char *cnt{nullptr};
   auto allocator = rm.getAllocator("UM");
